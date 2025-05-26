e0cda1bd1c13b79b6a624835bbde42e2e46719c5
dana gonzalez <dana.gonzalezdg1999@gmail.com>
2025-05-26 19:40:19 -0300

    numero invertido ej.1

--- Program.cs
+++ Program.cs
@@ -6,6 +6,32 @@ a=10;
 b=a;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:"+b);*/
+Console.Write("Ingrese un numero: ");
+    string numIng = Console.ReadLine();
+
+    if (int.TryParse(numIng, out int numero))//ingresa un string y sale (out) un tipo int
+        {
+            if (numero > 0)
+            {
+                int numeroInvertido = 0;
+                while (numero > 0)
+                {
+                    int digito = numero % 10;
+                    numeroInvertido = numeroInvertido * 10 + digito;
+                    numero /= 10;
+                }
+
+                Console.WriteLine($"Numero invertido: {numeroInvertido}");
+            }
+            else
+            {
+                Console.WriteLine("El numero debe ser mayor a 0.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada invalida, por favor ingrese un numero.");
+        }
 
 
 
